d43ce68fc69aa02a98a00c7dfa5e5195aa800d83
Dheeraj Varshney <dheerajvarshney@mail.com>
2021-12-23 19:51:38 +0400

    Fixed build issue

--- next.config.js
+++ next.config.js
@@ -1,12 +1,15 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-  
+  reactStrictMode: true,
+  swcMinify: true,
+  transpileModules: ["react-bulma-components"],
+  sassLoaderOptions: {
+    includePaths: ["./src/components"]
+  },
   cssModules: true,
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
     ignoreDuringBuilds: true,
-  },
-  swcMinify: true,
-
+  }
 }
